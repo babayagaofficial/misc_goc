0aa04d678cd2cc59995bc3520e232d44bbef6359
Daria Frolova <64833238+babayagaofficial@users.noreply.github.com>
2025-04-02 16:43:03 +0100

    Add files via upload

--- pangraph_workflow/fitch.py
+++ pangraph_workflow/fitch.py
@@ -17,56 +17,66 @@ def fitch_bottom_up(node):
         node.add_feature("possible_states", dict())
 
         # Intersection if possible, otherwise union
-        for block in left_states.keys(): #infer for each block independently
-            node.possible_states[block] = left_states[block] & right_states[block] if left_states[block] & right_states[block] else left_states[block] | right_states[block]
+        node.possible_states = left_states & right_states if left_states & right_states else left_states | right_states
 
     return node.possible_states
 
-def fitch_top_down(node, parent=None):
+def single_fitch_top_down(node, parent=None):
     """Top-down pass using the six-step Fitch algorithm to assign final states."""        
     
     if node.is_leaf():
         node.add_feature("final_states", node.possible_states)
-
-    # Step 1: For the root, choose arbitrary count for ambiguous blocks
     elif parent is None:
-        node.add_feature("final_states", dict())
-        for block in node.possible_states.keys():
-            node.final_states[block] = {node.possible_states[block].pop()}
+        pass
     else:
         node.add_feature("final_states", dict())
-        for block in node.possible_states.keys(): #infer for each block independently
-            # Step I: Check if preliminary nodal set ⊆ parent final set
-            if parent.final_states[block].issubset(node.possible_states[block]):
-                # Step II: Eliminate markers not in parent’s final set
-                node.final_states[block] = node.possible_states[block].intersection(parent.final_states[block])
+        # Step I: Check if preliminary nodal set ⊆ parent final set
+        if parent.final_states.issubset(node.possible_states):
+            # Step II: Eliminate markers not in parent’s final set
+            node.final_states = node.possible_states.intersection(parent.final_states)
+        else:
+            # Step III: Determine whether it was a union or intersection
+            left_states = node.children[0].possible_states
+            right_states = node.children[1].possible_states
+            was_union = not left_states.intersection(right_states)
+            
+            if was_union:
+                # Step IV: Union case
+                final_set = node.possible_states.union(parent.final_states)
             else:
-                # Step III: Determine whether it was a union or intersection
-                left_states = node.children[0].possible_states[block]
-                right_states = node.children[1].possible_states[block]
-                was_union = not left_states.intersection(right_states)
-                
-                if was_union:
-                    # Step IV: Union case
-                    final_set = node.possible_states[block].union(parent.final_states[block])
-                else:
-                    # Step V: Intersection case
-                    final_set = node.possible_states[block].copy()
-                    for state in parent.final_states[block]:
-                        if state in left_states or state in right_states:
-                            final_set.add(state)
-                node.final_states[block] = final_set
+                # Step V: Intersection case
+                final_set = node.possible_states.copy()
+                for state in parent.final_states:
+                    if state in left_states or state in right_states:
+                        final_set.add(state)
+            node.final_states = final_set
                 
     
     # Recursively process child nodes
     for child in node.children:
-        fitch_top_down(child, node)
+        single_fitch_top_down(child, node)
+
+def fitch_top_down(tree):
+    root = tree.get_tree_root()
+    recon_trees = []
+    while root.possible_states: #Want to reconstruct all possible trees, so go through all elements in candidate set of root
+        recon_tree = tree.copy()
+        # Step 1: For the root, choose arbitrary count for ambiguous blocks
+        recon_root = recon_tree.get_tree_root()
+        recon_root.add_feature("final_states", dict())
+        recon_root.final_states = {root.possible_states.pop()}
+        single_fitch_top_down(recon_root)
+        recon_trees.append(recon_tree)
+    return recon_trees
+
+
 
 def fitch_reconstruction(tree):
     """Executes both phases of Fitch's algorithm on an ete3 Tree."""
     root = tree.get_tree_root()
     fitch_bottom_up(root)
-    fitch_top_down(root)
+    recon_trees = fitch_top_down(root)
+    return recon_trees
 
 def print_tree(node, level=0):
     """Utility function to print the tree with assigned states."""
@@ -83,17 +93,20 @@ def read_tree(filepath): #read in Newick tree with unlabelled internal nodes, an
             i+=1
     return t
 
-def read_block_counts(pangraph_path, tree):
+def read_block_counts(pangraph_path, tree, min_len):
     graph = pp.Pangraph.from_json(pangraph_path)
-    bl_count = graph.to_blockcount_df()
     stats_df = graph.to_blockstats_df()
+    bl_count = graph.to_blockcount_df()
+    bl_count = bl_count.loc[bl_count.index.isin(stats_df[(stats_df["len"]>min_len) & (stats_df["core"]==False)].index)]
     chr_to_plasmid = {chr.name:plasmid for chr in tree.iter_leaves() for plasmid in bl_count.columns if chr.name in plasmid}
-    for leaf in tree.iter_leaves():
-        states = {block:{int(bl_count.loc[block, chr_to_plasmid[leaf.name]])} for block in bl_count.index}
-        leaf.add_feature("states", states)
-    return bl_count
+    all_trees = {block:tree.copy() for block in bl_count.index}
+    for block in all_trees.keys(): 
+        for leaf in all_trees[block].iter_leaves():
+            states = {bl_count.loc[block, chr_to_plasmid[leaf.name]]}
+            leaf.add_feature("states", states)
+    return list(stats_df[stats_df["core"]==True].index), all_trees
 
-def tree_to_df(tree): #after reconstruction
+def tree_to_df(tree): #after reconstruction, get counts as a DataFrame for both ancestral genomes and leaves
     root = tree.get_tree_root()
     counts = {block:[] for block in root.final_states.keys()}
     index = []
@@ -105,20 +118,21 @@ def tree_to_df(tree): #after reconstruction
     return df
 
 
-def tree_to_tsv(tree, block_counts, tsv):
-    chr_to_plasmid = {chr.name:plasmid for chr in tree.iter_leaves() for plasmid in bl_count.columns if chr.name in plasmid}
+def tree_to_tsv(recon_trees, core, tsv): #unfinished
     with open(tsv, "w") as f:
         f.write("genome\tfamily\tlower\thigher\n")
         for node in tree.traverse():
-            state_string = ','.join([str(state) for state in list(node.final_states)])
-            f.write(f"{node.name}\t{state_string}")
-            subtree = tree&node.name
-            leaves = [leaf.name for leaf in subtree.get_leaves()]
-            lower = len(node.final_states)
-            #upper = max([block_counts[block_counts.index.isin(list(node.final_states))][chr_to_plasmid[leaf]].sum() for leaf in leaves])
-            upper = sum([max(block_counts[[chr_to_plasmid[leaf] for leaf in leaves]].loc[state].values) for state in list(node.final_states)])
-            print(upper)
-            f.write(f"\t{lower}\t{upper}\n")
+            for block in recon_trees.keys():
+                f.write(f"{node.name}\t{block}\t")
+                counts = [(tree&node.name).final_states.pop() for tree in recon_trees[block]]
+                lower = min(counts)
+                upper = max(counts)
+                f.write(f"{lower}\t{upper}\n")
+            for block in core:
+                f.write(f"{node.name}\t{block}\t1\t1\n")
+
+
+
 
 
 # Example usage: Constructing a tree in Newick format
@@ -147,10 +161,10 @@ print_tree(tree)
 '''
 
 cluster_path = "/home/daria/Documents/projects/ABC/clades/lists"
-#clusters = [os.path.basename(el).replace('.txt','') for el in glob.glob(f"{cluster_path}/*.txt")]
-clusters = ["st131_cl416_community_0_subcommunity_503"]
-#clusters.remove("st69_cl461_community_0_subcommunity_499")
-#clusters.remove("st95_cl461_community_0_subcommunity_502")
+clusters = [os.path.basename(el).replace('.txt','') for el in glob.glob(f"{cluster_path}/*.txt")]
+#clusters = ["st131_cl416_community_0_subcommunity_503"]
+clusters.remove("st69_cl461_community_0_subcommunity_499")
+clusters.remove("st95_cl461_community_0_subcommunity_502")
 
 for cluster in clusters:
     print(cluster)
@@ -158,16 +172,16 @@ for cluster in clusters:
     tree = read_tree(filepath)
 
     pangraph = f"/home/daria/Documents/projects/ABC/pangraph_workflow/pangraph/{cluster}/pangraph.json"
-    bl_count = read_block_counts(pangraph, tree)
-    print(bl_count.transpose())
+    core, all_trees = read_block_counts(pangraph, tree, 200)
+
     # Run Fitch's method
-    fitch_reconstruction(tree)
+    recon_trees = {}
+    for block in all_trees.keys():
+        recon_trees[block] = fitch_reconstruction(all_trees[block])
+        # Print reconstructed tree states
+        #for tree in recon_trees[block]:
+        #    print(block)
+        #    print_tree(tree)
 
-    df = tree_to_df(tree)
-    flip = df.transpose()
-    print(len(flip[flip["I_1"]>0]["I_1"]))
-
-    # Print reconstructed tree states
-    #print_tree(tree)
-    #tree_to_tsv(tree,bl_count,f"fitch/ranges/{cluster}_range.tsv")
+    tree_to_tsv(recon_trees,core,f"fitch/ranges/{cluster}_range.tsv")
     #tree.write(format=1, outfile=f"fitch/relabelled_trees/{cluster}.nw")
